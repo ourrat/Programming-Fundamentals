f7e18bc9904d2f05d4fedd6e1cfe891673954209
OurratMohamed <48866828+ourrat@users.noreply.github.com>
2025-10-26 15:14:35 -0400

    Add files via upload

---
+++ algorithm and problem-solving /algorithm and problem-solving Level 2/Problem 6.cpp
@@ -0,0 +1,28 @@
+// Problem 6.
+
+#include <iostream>
+using namespace std;
+
+int ReadNumber(string message) {
+    int number;
+    do {
+        cout << message;
+        cin >> number;
+    } while (number <= 0);
+    return number;
+}
+int SumOfDigits(int number) {
+    int Remainder = 0;
+    int Sum = 0;
+    while (number > 0) {
+        Remainder = number % 10;
+        number = number / 10;
+        Sum += Remainder;
+    }
+    return Sum;
+}
+int main()
+{
+    cout << "Sum Of Digits = " << SumOfDigits(ReadNumber("Please Enter a Positive Number: "));
+}
+
