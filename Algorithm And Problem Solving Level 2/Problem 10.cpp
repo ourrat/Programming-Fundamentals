49b3223f4aeb341bdc3322f3074a36e3f6ac56c0
OurratMohamed <48866828+ourrat@users.noreply.github.com>
2025-10-29 13:59:55 -0400

    Add files via upload

---
+++ Algorithm And Problem Solving Level 2/Problem 10.cpp
@@ -0,0 +1,35 @@
+// Problem 10.
+
+#include <iostream>
+using namespace std;
+
+int ReadNumber(string message) {
+    int Number;
+    do {
+        cout << message;
+        cin >> Number;
+    } while (Number <= 0);
+    return Number;
+}
+int ReverseNumber(int Number) {
+    int Remainder = 0, Number2 = 0;
+    while (Number > 0) {
+        Remainder = Number % 10;
+        Number = Number / 10;
+        Number2 = Number2 * 10 + Remainder;
+    }
+    return Number2;
+}
+void PrintDigits(int Number) {
+    int Remainder = 0;
+    while (Number > 0) {
+        Remainder =  Number % 10;
+        Number = Number / 10;
+        cout << Remainder << endl;
+    }
+}
+int main()
+{
+    int Number = ReadNumber("Please Enter a Positive Number : ");
+    PrintDigits(ReverseNumber(Number));
+}
