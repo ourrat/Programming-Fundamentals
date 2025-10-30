e8918be73e1b419f745e62e4594e2c0333bee9c2
OurratMohamed <48866828+ourrat@users.noreply.github.com>
2025-10-30 16:21:09 -0400

    Add files via upload

---
+++ Algorithm And Problem Solving Level 2/Problem 13.cpp
@@ -0,0 +1,25 @@
+// Problem 13.
+
+#include <iostream>
+using namespace std;
+
+int ReadNumber(string message) {
+    int Number;
+    do {
+        cout << message;
+        cin >> Number;
+    } while (Number <= 0);
+    return Number;
+}
+void PrintInvertedPattern(int Number) {
+    for (int i = 0; i <= Number; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    PrintInvertedPattern(ReadNumber("Please Enter a Positive Number: "));
+}
